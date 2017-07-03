5aea0037d54f223ac9b7f889b84f7bf9f903571a
CocoaHaha <1076800596@qq.com>
2017-07-03 17:43:53 +0800

    Initial Commit

---
+++ 高斯模糊/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  高斯模糊
+//
+//  Created by 石向锋 on 2017/7/3.
+//  Copyright © 2017年 CocoHaHa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 高斯模糊/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  高斯模糊
+//
+//  Created by 石向锋 on 2017/7/3.
+//  Copyright © 2017年 CocoHaHa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
